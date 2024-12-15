f532aa5082dcf1ff231e2a0817549b53186bc3a7
sysAdmin-I <komandurovizat@gmail.com>
2024-12-15 15:44:28 +0600

    new commit

--- js/home_works.js
+++ js/home_works.js
@@ -104,3 +104,42 @@
 // startBtn.onclick = function () {startSeconds()}
 // stopBtn.onclick = function () {stopSeconds()}
 // resetBtn.onclick = function () {resetSeconds()}
+
+// JSON
+
+const characterList = document.querySelector('.characters-list');
+
+const request = new XMLHttpRequest();
+request.open('GET', '../data/persons.json');
+request.setRequestHeader('Content-Type', 'application/json');
+request.send();
+
+request.onload = () => {
+    let data = JSON.parse(request.response);
+
+    data.forEach(character => {
+        const card = document.createElement('div');
+        card.classList.add('character-card');
+
+        card.innerHTML = `
+       <img src="${character.person_photo}" alt="${character.name}" />
+       <h2>${character.name}</h2>
+       <p>Age: ${character.age}</p>
+    `;
+        card.style.color = "white";
+        card.style.fontSize = "20px";
+        characterList.append(card)
+    });
+};
+
+const request1 = new XMLHttpRequest();
+request1.open('GET', '../data/any.json', );
+request1.send();
+
+
+request1.onload = () => {
+    let data1 = JSON.parse(request1.response);
+    console.log(data1);
+};
+
+
