452aa7abf92d7742853538e049f228550376014d
sysAdmin-I <komandurovizat@gmail.com>
2024-12-19 00:27:44 +0600

    New commit

--- js/lesson.js
+++ js/lesson.js
@@ -2,23 +2,21 @@
 
 // Phone block
 
-// const phoneInput = document.querySelector("#phone_input");
-// const phoneButton = document.querySelector("#phone_button");
-// const phoneResult = document.querySelector("#phone_result");
-//
-// const regExp = /^\+996 [2579]\d{2} \d{2}-\d{2}-\d{2}$/
-//
-// phoneButton.onclick = () => {
-//     if (regExp.test(phoneInput.value)) {
-//         phoneResult.innerHTML = 'OK'
-//         phoneResult.style.color = 'green'
-//     } else {
-//         phoneResult.innerHTML = 'invalid phone number'
-//         phoneResult.style.color = 'red'
-//     }
-// }
+const phoneInput = document.querySelector("#phone_input");
+const phoneButton = document.querySelector("#phone_button");
+const phoneResult = document.querySelector("#phone_result");
 
-//
+const regExp = /^\+996 [2579]\d{2} \d{2}-\d{2}-\d{2}$/
+
+phoneButton.onclick = () => {
+    if (regExp.test(phoneInput.value)) {
+        phoneResult.innerHTML = 'OK'
+        phoneResult.style.color = 'green'
+    } else {
+        phoneResult.innerHTML = 'invalid phone number'
+        phoneResult.style.color = 'red'
+    }
+}
 
 //tab slider
 
@@ -68,4 +66,46 @@ const autoTabs = () => {
 
 autoTabs()
 
+// Converter
 
+const somInput = document.querySelector('#som');
+const usdInput = document.querySelector('#usd');
+const eurInput = document.querySelector('#eur');
+
+const converter = (element, targetElement,secondElement,threeElement,fourElement) => {
+    element.oninput = () => {
+        console.log(targetElement.id)
+        const xhr = new XMLHttpRequest()
+        xhr.open('GET', '../data/converter.json')
+        xhr.setRequestHeader('Content-type', 'application/json')
+        xhr.send()
+
+        xhr.onload = () => {
+
+            const data = JSON.parse(xhr.response)
+
+            if (element.id === 'som') {
+                targetElement.value = (element.value / data.usd).toFixed(2);
+                secondElement.value = (element.value / data.eur).toFixed(2);
+            }
+            if (element.id === 'usd') {
+                targetElement.value = (element.value * data.usd).toFixed(2);
+                secondElement.value = ((element.value * data.usd) / data.eur).toFixed(2);
+            }
+            if (element.id === 'eur') {
+                targetElement.value = (element.value * data.eur).toFixed(2);
+                secondElement.value = ((element.value * data.eur) / data.usd).toFixed(2);
+            }
+
+            if (element.value === '') {targetElement.value = '';secondElement.value = '';threeElement.value = '';fourElement.value = '';}
+        }
+    }
+}
+
+converter(somInput, usdInput, eurInput);
+converter(usdInput, somInput, eurInput);
+converter(eurInput, somInput, usdInput);
+
+
+// DRY - don't repeat yourself
+// KISS - keep it super simple
--- js/modal.js
+++ js/modal.js
@@ -1,33 +1,33 @@
 // modal
 
-const modal = document.querySelector(".modal");
-const modalTrigger = document.querySelector("#btn-get");
-const modalCloseButton = document.querySelector(".modal_close");
-
-const openModal = () => {
-    modal.style.display = "block";
-    document.body.style.overflow = "hidden";
-}
-const closeModal = () => {
-    modal.style.display = "none";
-    document.body.style.overflow = "auto";
-}
-
-modalTrigger.onclick = openModal;
-modalCloseButton.onclick = closeModal;
-modal.onclick = (event) => {
-   if (event.target === modal) {
-       closeModal();
-   }
-}
-
-const scrollBottom = () => {
-    if (window.innerHeight + window.scrollY >= document.documentElement.scrollHeight) {
-        openModal();
-        window.removeEventListener("scroll", scrollBottom);
-    }
-};
-
-window.addEventListener("scroll", scrollBottom);
-
-setTimeout(openModal, 10000)
+// const modal = document.querySelector(".modal");
+// const modalTrigger = document.querySelector("#btn-get");
+// const modalCloseButton = document.querySelector(".modal_close");
+//
+// const openModal = () => {
+//     modal.style.display = "block";
+//     document.body.style.overflow = "hidden";
+// }
+// const closeModal = () => {
+//     modal.style.display = "none";
+//     document.body.style.overflow = "auto";
+// }
+//
+// modalTrigger.onclick = openModal;
+// modalCloseButton.onclick = closeModal;
+// modal.onclick = (event) => {
+//    if (event.target === modal) {
+//        closeModal();
+//    }
+// }
+//
+// const scrollBottom = () => {
+//     if (window.innerHeight + window.scrollY >= document.documentElement.scrollHeight) {
+//         openModal();
+//         window.removeEventListener("scroll", scrollBottom);
+//     }
+// };
+//
+// window.addEventListener("scroll", scrollBottom);
+//
+// setTimeout(openModal, 10000)
